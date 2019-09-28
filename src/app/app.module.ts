4f17301ad280277b3ecd3d963384f4b1e16c78db
mrsfreddy <rmogusu123@gmal.com>
2019-09-28 08:47:11 +0300

    Generate user data component to handle the logic of the users data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDataComponent } from './user-data/user-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDataComponent
   ],
   imports: [
     BrowserModule,
