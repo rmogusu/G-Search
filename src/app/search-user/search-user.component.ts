b91a654ffe5c834e0dc624a3126cfaa70372382a
mrsfreddy <rmogusu123@gmal.com>
2019-09-28 13:05:10 +0300

    Writte a function that will get values from form inputs after its being submited

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter, } from '@angular/core';
 
 @Component({
   selector: 'app-search-user',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-user.component.css']
 })
 export class SearchUserComponent implements OnInit {
+  searchTerm: string;
+  @Output() searchEmmiter = new EventEmitter<any>();
 
   constructor() { }
 
+emmitUser() {
+  this.searchEmmiter.emit(this.searchTerm);
+  }
   ngOnInit() {
   }
 
