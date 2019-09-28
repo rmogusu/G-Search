bdade7550b5c196f46555a51c3ad426449cf24c2
mrsfreddy <rmogusu123@gmal.com>
2019-09-28 08:37:43 +0300

    Generate repo model class where i will define my repo properties and attribute them

--- src/app/git-user.ts
+++ src/app/git-user.ts
@@ -1,2 +1,13 @@
 export class GitUser {
+    avatar_url: any;
+    login: any;
+    bio: any;
+    name: any;
+    company: any;
+    location: any;
+    followers: any;
+    following: any;
+    created_at: Date;
+    public_repos: any;
+
 }
