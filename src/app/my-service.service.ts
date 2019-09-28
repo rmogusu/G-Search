539880fb1b20426ee3a99ac4fe847a8d6c1d56b3
mrsfreddy <rmogusu123@gmal.com>
2019-09-28 11:24:30 +0300

    Create a service to implent Indepency Injection

---
+++ src/app/my-service.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {GitUser} from './git-user';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class MyServiceService {
+
+  constructor() { }
+}
